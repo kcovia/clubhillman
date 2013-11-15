130aa0851716d26ee39ab057f055eb42029a2f07
Kiefer Covia <kcovia@gmail.com>
2013-11-15 10:22:15 -0500

    Complete redesign

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+
+
+function showDesign () {
+	document.getElementById('design-big').style.display = "block";
+	document.getElementById('tshirt-big').style.display = "none";
+
+}
+
+function showShirt() {
+	document.getElementById('tshirt-big').style.display = "block";
+	document.getElementById('design-big').style.display = "none";
+
+}
